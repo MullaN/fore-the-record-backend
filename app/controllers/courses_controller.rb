073078adc4e74ac82442d7da82d4756923a64511
MullaN <admullan@gmail.com>
2020-11-24 12:46:02 -0800

    added courses show route

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,4 +4,9 @@ class CoursesController < ApplicationController
         courses = courses.sort_by {|course| course.num_rounds}.reverse
         render json: courses.to_json(only: ['id', 'name', 'cover_img'], methods: ['num_rounds'])
     end
+
+    def show
+        course = Course.find(params[:id])
+        render json: courses.to_json(only: ['id', 'name', 'cover_img'], methods: ['top_ten_rounds'])
+    end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -11,7 +11,7 @@ class Course < ApplicationRecord
         return self.hole_1 + self.hole_2 + self.hole_3 + self.hole_4 + self.hole_5 + self.hole_6 + self.hole_7 + self.hole_8 + self.hole_9 + self.hole_10 + self.hole_11 + self.hole_12 + self.hole_13 + self.hole_14 + self.hole_15 + self.hole_16 + self.hole_17 + self.hole_18
     end
 
-    def first_ten_rounds
-        
+    def top_ten_rounds
+        return self.rounds.sort_by {|round| round.score }[0..10]
     end
 end
