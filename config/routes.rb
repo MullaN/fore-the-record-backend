02a17ddde9d3c51370ff2cb47dfee59623b3fc0d
MullaN <admullan@gmail.com>
2020-11-24 12:48:43 -0800

    added courses show route to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :rounds, only: [:index]
   resources :matches
-  resources :courses, only: [:index]
+  resources :courses, only: [:index, :show]
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
