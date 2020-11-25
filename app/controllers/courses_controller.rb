64389ebc8c4dc74dc364cc39682c342aef8ca350
MullaN <admullan@gmail.com>
2020-11-24 21:09:20 -0800

    changed course index json

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,6 +2,6 @@ class CoursesController < ApplicationController
     def index
         courses = Course.all
         courses = courses.sort_by {|course| course.num_rounds}.reverse
-        render json: courses.to_json(only: ['id', 'name', 'cover_img'], methods: ['num_rounds'])
+        render json: courses.to_json(except:[:created_at, :updated_at, :description], methods: ['num_rounds'])
     end
 end
