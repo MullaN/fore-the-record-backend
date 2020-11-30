92787507804ed47ac1fc5884f1cbfc4d79e4d494
MullaN <admullan@gmail.com>
2020-11-29 16:56:51 -0800

    fixed user methods for no rounds

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,31 +8,47 @@ class User < ApplicationRecord
     has_secure_password
 
     def handicap
-        best_eight = self.last_twenty.sort_by {|round| round.score.to_i}[0..7]
-        return -(best_eight.sum {|round| round.score.to_i} / 8)
+        if self.rounds.length > 0
+            best_eight = self.last_twenty.sort_by {|round| round.score.to_i}[0..7]
+            return -(best_eight.sum {|round| round.score.to_i} / 8)
+        else
+            return 'N/A'
+        end
     end
 
     def best_round
-        best_round = self.rounds.sort_by{|round| round.score.to_i}[0]
-        return {round: best_round, score: best_round.score, course: best_round.course}
+        if self.rounds.length > 0
+            best_round = self.rounds.sort_by{|round| round.score.to_i}[0]
+            return {round: best_round, score: best_round.score, course: best_round.course}
+        else
+            return {}
+        end
     end
 
     def best_course
-        course_track = {}
-        self.rounds.each do |round|
-            if (course_track[round.course.id])
-                course_track[round.course.id][:total] += round.score.to_i
-                course_track[round.course.id][:num_rounds] += 1
-                course_track[round.course.id][:average] = course_track[round.course.id][:total] / course_track[round.course.id][:num_rounds]
-            else
-                course_track[round.course.id] = {total: round.score.to_i, num_rounds: 1, average: round.score.to_i}
+        if self.rounds.length > 0
+            course_track = {}
+            self.rounds.each do |round|
+                if (course_track[round.course.id])
+                    course_track[round.course.id][:total] += round.score.to_i
+                    course_track[round.course.id][:num_rounds] += 1
+                    course_track[round.course.id][:average] = course_track[round.course.id][:total] / course_track[round.course.id][:num_rounds]
+                else
+                    course_track[round.course.id] = {total: round.score.to_i, num_rounds: 1, average: round.score.to_i}
+                end
             end
-        end
 
-        return Course.find(course_track.min_by {|k, v| v[:average]}[0])
+            return Course.find(course_track.min_by {|k, v| v[:average]}[0])
+        else
+            return {}
+        end
     end
 
     def last_twenty
-        return self.rounds.sort_by {|round| round.created_at}.reverse[0..19]
+        if self.rounds.length > 0
+            return self.rounds.sort_by {|round| round.created_at}.reverse[0..19]
+        else
+            return []
+        end
     end
 end
