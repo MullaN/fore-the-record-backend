5892e406889da0d510a4fda507374bc64caf3c22
MullaN <admullan@gmail.com>
2020-12-07 09:24:22 -0800

    fixed routes typo

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :rounds, only: [:index]
   resources :matches, only: [:create]
   resources :courses, only: [:index, :show]
-  resources :users, only [:create, :show]
+  resources :users, only: [:create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/users/:id/rounds', to: 'users#rounds' 
