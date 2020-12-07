0fc9b579667421c52de6da13c8de33092a43b4f0
MullaN <admullan@gmail.com>
2020-12-07 09:22:23 -0800

    still trying to fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://fore-the-record.web.app'
+    origins 'localhost:3000', 'fore-the-record.web.app'
 
     resource '*',
       headers: :any,
